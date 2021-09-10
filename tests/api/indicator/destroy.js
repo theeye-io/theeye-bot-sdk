cc5ca836259056d269fbfffac4bf738d1f8c6e5b
Facundo <facugon@theeye.io>
2021-09-10 09:40:02 -0300

    update api url

--- tests/api/indicator/destroy.js
+++ tests/api/indicator/destroy.js
@@ -1,7 +1,7 @@
 
 const Indicators = require('../../../core/api/indicator')
 const accessToken = process.env.ACCESS_TOKEN
-const baseUrl = 'http://127.0.0.1:60080'
+const baseUrl = 'https//supervisor.theeye.io'
 
 const main = async () => {
 
